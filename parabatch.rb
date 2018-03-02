574ebee88694eec9191d06add3ab91a97f4348df
Masaomi Hatakeyama <masaomi.hatakeyama@gmail.com>
2018-03-02 11:32:13 +0100

    Fixed parabatch.rb

--- parabatch.rb
+++ parabatch.rb
@@ -5,14 +5,14 @@ require 'parallel'
 require 'ruby-progressbar'
 
 unless batch_file=ARGV[0] and threads=ARGV[1]
-  puts "Usage:\n ruby #{File.basename(__FILE__)} [batch.sh] [threads] ([out.dat]) 2> err.log"
+  puts "Usage:\n ruby #{File.basename(__FILE__)} [batch.sh] [threads] ([out.dat])"
   exit
 end
 threads = threads.to_i
 out_file = ARGV[2]||"out.dat"
 
 commands = File.readlines(batch_file).to_a.select{|line| !line.chomp.empty?}
-progress = ProgressBar.create(title: "Progress", total: commands.length, format: '%a %B %p%% %t')
+progress = ProgressBar.create(title: "Progress", total: commands.length, format: '%a %B %p%% %t', output: $stderr)
 result = Parallel.map(commands, in_threads: threads, finish: -> (item, i, res){ progress.increment }) do |command|
   `#{command}`
 end
